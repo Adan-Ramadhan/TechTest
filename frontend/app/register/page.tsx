76c130caa5079b841da87a6e3e29d6620ccbf4d6
Adan <adanramadhan2001@gmail.com>
2025-02-19 11:22:12 +0700

    create register page

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,68 +1,63 @@
 "use client";
 
-import { storage } from "@/utils/storage";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const Login = () => {
-  const base_url = process.env.NEXT_PUBLIC_API_URL;
-
+const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
+  const base_url = process.env.NEXT_PUBLIC_API_URL;
 
-  const handleFormLogin = async (e: React.FormEvent) => {
+  const register = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${base_url}/login`, {
+      const response = await axios.post(`${base_url}/register`, {
         email,
         password,
       });
 
-      storage.save("token", response.data.token);
       console.log(response);
+
       router.push("/dashboard");
     } catch (err) {
-      console.error("gagal logiin", err);
+      console.error("Gagal register", err);
     }
   };
 
   return (
     <div className="w-full h-screen">
       <div className="w-full h-full flex justify-center items-center">
-        <form
-          onSubmit={handleFormLogin}
-          className="rounded shadow flex flex-col p-5"
-        >
-          <h1 className="font-bold text-2xl mb-3">Login</h1>
+        <form onSubmit={register} className="rounded shadow flex flex-col p-5">
+          <h1 className="font-bold text-2xl mb-3">Register</h1>
           <label>Email</label>
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="border-2 rounded mb-3"
+            className="w-full border-2 rounded mb-3"
           />
           <label>Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border-2 rounded mb-3"
+            className="w-full border-2 rounded mb-3"
           />
           <button
             type="submit"
-            className="bg-slate-700 py-1 rounded font-bold text-white mb-3"
+            className="bg-slate-700 py-1 rounded font-bold mb-3 text-white"
           >
             Submit
           </button>
           <p className="text-sm text-center">
-            Don&apos;t have an account?{" "}
-            <Link href="/register" className="text-sky-500">
+            Have an account?{" "}
+            <Link href="/login" className="text-sky-500">
               {" "}
-              register
+              login
             </Link>
           </p>
         </form>
@@ -71,4 +66,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
